070917f3bc8b42d6b1bdcde1d36567c6e974419d
matias <matias@DESKTOP-3O1V6FN>
2023-09-21 20:29:13 +0100

    se ha agregado estructura de carpetas

--- Sistema_optica/CapaPresentacion/Vendedor/inicio_vendedor.Designer.cs
+++ Sistema_optica/CapaPresentacion/Vendedor/inicio_vendedor.Designer.cs
@@ -29,11 +29,36 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
+            bindingSource1 = new BindingSource(components);
+            menuStrip1 = new MenuStrip();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(584, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // inicio_vendedor
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            ClientSize = new Size(584, 361);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "inicio_vendedor";
+            Text = "Inicio de sesion";
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private BindingSource bindingSource1;
+        private MenuStrip menuStrip1;
     }
 }
