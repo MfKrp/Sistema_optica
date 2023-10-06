f8ee7868fa1167b3e0167a165e228302e584fc31
MfKrp <68623283+MfKrp@users.noreply.github.com>
2023-10-06 15:03:31 +0100

    Add files via upload

---
+++ Sistema_optica/Capa_Entidad/Factura.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Factura
+    {
+        public int Nro_factura { get; set; }
+        public DateTime Fecha_factura { get; set; }
+        public UsuarioEmpleado Vendedor { get; set; }
+        public Cliente cliente { get; set; }
+        public Forma_pago Tipo_pago { get; set; }
+    }
+}
