5cafb74a6540e787fb481e71fefe1ab2b036ebcf
MfKrp <68623283+MfKrp@users.noreply.github.com>
2023-10-06 15:43:51 +0100

    Add files via upload

---
+++ Sistema_optica/CapaEntidad/Detalle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Detalle
+    {
+        public int Id_detalle { get; set; }
+        public float Precio_total { get; set; }
+        public int Cantidad { get; set; }
+        public Producto ID_producto { get; set; }
+        public Factura ID_factura { get; set; }
+    }
+}
