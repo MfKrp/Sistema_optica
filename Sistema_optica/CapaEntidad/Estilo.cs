50fbd1c2bec1f696a4d1bcce364b459b877a9d98
MfKrp <68623283+MfKrp@users.noreply.github.com>
2023-11-17 14:41:42 +0100

    Add files via upload

---
+++ Sistema_optica/CapaEntidad/Estilo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Estilo
+    {
+        public int ID_estilo { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
---
+++ Sistema_optica/CapaEntidad/Forma_pago.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class Forma_pago
+    {
+        public int Id_tipo_pago { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
